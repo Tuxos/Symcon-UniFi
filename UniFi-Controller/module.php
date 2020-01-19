d460d08e621bb3d6c8a7f91e71ff974d3bafd2eb
Dennis Deitermann <Tuxos@users.noreply.github.com>
2020-01-19 23:12:21 +0100

    Update module.php

--- UniFi-Controller/module.php
+++ UniFi-Controller/module.php
@@ -43,8 +43,8 @@
 
             $unifi_connection = new UniFi_API\Client($username, $password, $url, $site, $version, false);
             $login = $unifi_connection->login();
-            // $results = $unifi_connection->disable_wlan("5c434b0bba3e820de56caf19", false);
-            $results = $unifi_connection->$command; // returns a PHP array containing alarm objects
+            $results = $unifi_connection->disable_wlan("5c434b0bba3e820de56caf19", false);
+            // $results = $unifi_connection->$command; // returns a PHP array containing alarm objects
 
             return var_dump($results);
         }
