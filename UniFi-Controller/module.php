e2821ee02a1330e32254d0c4e12aef87e9c5fda0
Dennis Deitermann <Tuxos@users.noreply.github.com>
2020-01-19 22:08:59 +0100

    Create module.php

---
+++ UniFi-Controller/module.php
@@ -0,0 +1,57 @@
+<?php
+    
+    class UniFiController extends IPSModule {
+ 
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+            // Diese Zeile nicht löschen.
+            parent::Create();
+ 
+            $this->RegisterPropertyString("url", "https://192.168.1.7:8443");
+            $this->RegisterPropertyString("username", "api_user");
+            $this->RegisterPropertyString("password", "");
+            $this->RegisterPropertyString("site", "default");
+            $this->RegisterPropertyString("version", "5.12.35");
+
+        }
+ 
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+
+            $this->RegisterVariableBoolean("online", "Online", "~Switch",1);
+
+        }
+ 
+        /**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * ABC_MeineErsteEigeneFunktion($id);
+        *
+        */
+        public function callapi(string $command ) {
+
+            include_once(__DIR__ . "../libs/Client.php");
+
+            $url = $this->ReadPropertyString("url");
+            $username = $this->ReadPropertyString("username");
+            $password = $this->ReadPropertyString("password");
+            $site = $this->ReadPropertyString("site");
+            $version = $this->ReadPropertyString("version");
+
+            $unifi_connection = new UniFi_API\Client($username, $password, $url, $site, $version, false);
+            $login = $unifi_connection->login();
+            $results = $unifi_connection->$command; // returns a PHP array containing alarm objects
+
+            return var_dump($results);
+        }
+
+        public function disable_wLan( $wlanid, $bool ) {
+
+            return UC_callapi('disable_wlan("'.$wlanid.'", ".$bool.")');
+        }
+    }
+
+?>
