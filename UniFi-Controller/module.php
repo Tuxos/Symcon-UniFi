aecd2675ef7d3ea46a50f904456f1943cef4a815
Dennis Deitermann <Tuxos@users.noreply.github.com>
2020-01-20 01:22:02 +0100

    Update module.php

--- UniFi-Controller/module.php
+++ UniFi-Controller/module.php
@@ -25,7 +25,7 @@
 
             $this->RegisterVariableBoolean("online", "Online", "~Switch",1);
 
-            $this->RegisterTimer('Readdata', $this->ReadPropertyInteger("intervall"), 'UNIFI_login_test($id)');
+            $this->RegisterTimer('Update', $this->ReadPropertyInteger('intervall'), 'UNIFI_login_test($id);');
 
             if (UNIFI_login_test($this->InstanceID) == "true")
 			{
@@ -36,35 +36,34 @@
 			}
         }
  
-	// Erstelle Events
-	protected function RegisterTimer($ident, $interval, $script) {
+	    /*protected function RegisterTimer($ident, $interval, $script) {
 
-		$id = @IPS_GetObjectIDByIdent($ident, $this->InstanceID);
+		    $id = @IPS_GetObjectIDByIdent($ident, $this->InstanceID);
 
-		if ($id && IPS_GetEvent($id)['EventType'] <> 1) {
-			IPS_DeleteEvent($id);
-			$id = 0;
-		}
+		    if ($id && IPS_GetEvent($id)['EventType'] <> 1) {
+		    	IPS_DeleteEvent($id);
+		    	$id = 0;
+		    }
 
-		if (!$id) {
-			$id = IPS_CreateEvent(1);
-			IPS_SetParent($id, $this->InstanceID);
-			IPS_SetIdent($id, $ident);
-		}
+		    if (!$id) {
+			    $id = IPS_CreateEvent(1);
+			    IPS_SetParent($id, $this->InstanceID);
+		    	IPS_SetIdent($id, $ident);
+		    }
 
-		IPS_SetName($id, $ident);
-		IPS_SetHidden($id, true);
-		IPS_SetEventScript($id, "\$id = \$_IPS['TARGET'];\n$script;");
-		if (!IPS_EventExists($id)) throw new Exception("Ident with name $ident is used for wrong object type");
+		    IPS_SetName($id, $ident);
+		    IPS_SetHidden($id, true);
+		    IPS_SetEventScript($id, "\$id = \$_IPS['TARGET'];\n$script;");
+		    if (!IPS_EventExists($id)) throw new Exception("Ident with name $ident is used for wrong object type");
 
-		if (!($interval > 0)) {
-			IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, 1);
-			IPS_SetEventActive($id, false);
-		} else {
-			IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $interval);
-			IPS_SetEventActive($id, true);
-		}
-	}
+		    if (!($interval > 0)) {
+		    	IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, 1);
+		    	IPS_SetEventActive($id, false);
+		    } else {
+		    	IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $interval);
+		    	IPS_SetEventActive($id, true);
+		    }
+	    }*/
 
         public function disable_wlan($wlanid, $bool) {
 
