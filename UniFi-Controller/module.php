e766df9d73d8796d49b55f88c43b3b0bb8f0df56
Dennis Deitermann <Tuxos@users.noreply.github.com>
2020-01-20 01:10:41 +0100

    Update module.php

--- UniFi-Controller/module.php
+++ UniFi-Controller/module.php
@@ -14,6 +14,7 @@
             $this->RegisterPropertyString("password", "");
             $this->RegisterPropertyString("site", "default");
             $this->RegisterPropertyString("version", "5.12.35");
+            $this->RegisterPropertyInteger("intervall", "60");
 
         }
  
@@ -24,6 +25,8 @@
 
             $this->RegisterVariableBoolean("online", "Online", "~Switch",1);
 
+            $this->RegisterTimer('Readdata', $this->ReadPropertyInteger("intervall"), 'UNIFI_login_test($id)');
+
             if (UNIFI_login_test($this->InstanceID) == "true")
 			{
 				$this->SetStatus(102);
@@ -33,13 +36,35 @@
 			}
         }
  
-        /**
-        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
-        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
-        *
-        * ABC_MeineErsteEigeneFunktion($id);
-        *
-        */
+	// Erstelle Events
+	protected function RegisterTimer($ident, $interval, $script) {
+
+		$id = @IPS_GetObjectIDByIdent($ident, $this->InstanceID);
+
+		if ($id && IPS_GetEvent($id)['EventType'] <> 1) {
+			IPS_DeleteEvent($id);
+			$id = 0;
+		}
+
+		if (!$id) {
+			$id = IPS_CreateEvent(1);
+			IPS_SetParent($id, $this->InstanceID);
+			IPS_SetIdent($id, $ident);
+		}
+
+		IPS_SetName($id, $ident);
+		IPS_SetHidden($id, true);
+		IPS_SetEventScript($id, "\$id = \$_IPS['TARGET'];\n$script;");
+		if (!IPS_EventExists($id)) throw new Exception("Ident with name $ident is used for wrong object type");
+
+		if (!($interval > 0)) {
+			IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, 1);
+			IPS_SetEventActive($id, false);
+		} else {
+			IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $interval);
+			IPS_SetEventActive($id, true);
+		}
+	}
 
         public function disable_wlan($wlanid, $bool) {
 
