1a04080f6e5be240a4d4401d3398a01f2a620bd0
Dennis Deitermann <Tuxos@users.noreply.github.com>
2020-01-21 12:40:11 +0100

    Update UNIFI_wlan-action-script.php

--- libs/UNIFI_wlan-action-script.php
+++ libs/UNIFI_wlan-action-script.php
@@ -2,12 +2,21 @@
 
     SetValue($_IPS['VARIABLE'], $_IPS['VALUE']);
 
-    if ($_IPS['VALUE'] == false)
+    $VarInf = IPS_GetVariable($_IPS['VARIABLE']);
+    
+    If ($VarInf['VariableType'] == 0)
       {
-        UNIFI_disable_wlan(IPS_GetParent(IPS_GetParent($_IPS['VARIABLE'])), GetValueString(IPS_GetVariableIDByName("wlan_id", $_IPS['VARIABLE'])), true);
-      } else
-      {
-        UNIFI_disable_wlan(IPS_GetParent(IPS_GetParent($_IPS['VARIABLE'])), GetValueString(IPS_GetVariableIDByName("wlan_id", $_IPS['VARIABLE'])), false);
+        if ($_IPS['VALUE'] == false)
+        {
+          UNIFI_disable_wlan(IPS_GetParent(IPS_GetParent($_IPS['VARIABLE'])), GetValueString(IPS_GetVariableIDByName("wlan_id", $_IPS['VARIABLE'])), true);
+        } else
+        {
+          UNIFI_disable_wlan(IPS_GetParent(IPS_GetParent($_IPS['VARIABLE'])), GetValueString(IPS_GetVariableIDByName("wlan_id", $_IPS['VARIABLE'])), false);
+        }
       }
-
+    
+    If ($VarInf['VariableType'] == 3)
+      {
+        UNIFI_set_wlansettings(IPS_GetParent(IPS_GetParent($_IPS['VARIABLE'])), GetValueString(IPS_GetVariableIDByName("wlan_id", $_IPS['VARIABLE'])), $_IPS['VALUE']);
+      }
 ?>
