673be2419e813104bc3f99662cdabe491df2826b
Dennis Deitermann <Tuxos@users.noreply.github.com>
2020-12-02 15:11:43 +0100

    Update module.php

--- UniFi-Controller/module.php
+++ UniFi-Controller/module.php
@@ -178,12 +178,12 @@
             }
 
             //$enable = array();
-            $enable->enabled = $bool;
-            $payload = json_encode($enable);
+            //$enable->enabled = $bool;
+            //$payload = json_encode($enable);
 
             $urlportfwd          = '/api/s/'.$site.'/rest/portforward/'.$portfwd_id;
             $request_type = 'POST';
-            //$payload      = '{"_id":"'.$portfwd_id.'","name":"Homematic","enabled":'.$bool2.',"src":"any","dst_port":"443","fwd":"192.168.1.2","fwd_port":"443","proto":"tcp_udp","log":true,"site_id":"5c4349c9ba3e820de56caf00","pfwd_interface":"wan"}';
+            $payload      = '{"_id":"'.$portfwd_id.'","name":"Homematic","enabled":'.$bool2.',"src":"any","dst_port":"443","fwd":"192.168.1.2","fwd_port":"443","proto":"tcp_udp","log":true,"site_id":"5c4349c9ba3e820de56caf00","pfwd_interface":"wan"}';
             //$payload      = '{"enabled": true}';
             $return       = 'array';
 
